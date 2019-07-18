940b4f9f3ff197af136c3eb3d5be9e44ee929a49
beellz <belal20795@gmail.com>
2019-07-18 17:08:14 +0530

    added floor value to give only two decimal

--- app.js
+++ app.js
@@ -8,14 +8,14 @@ function allClick() {
 
     if (per == "4%") {
         var k = ( B * 0.04);
-        var y = ( k / p);
+        var y = Math.floor(( k / p)*100 )/100;
         var j = "The Tip Amount To Be Paid is $" + y + " Dollars";
         document.getElementById("demo").innerHTML = j;
     } 
 
     if (per == "5%") {
         var k = ( B * 0.05);
-        var y = ( k / p);
+        var y = Math.floor(( k / p)*100 )/100;
         var j = "The Tip Amount To Be Paid is $" + y + " Dollars";
         document.getElementById("demo").innerHTML = j;
     } 
@@ -23,14 +23,14 @@ function allClick() {
 
     if (per == "10%") {
         var k = ( B * 0.1);
-        var y = ( k / p);
+        var y = Math.floor(( k / p)*100 )/100;
         var j = "The Tip Amount To Be Paid is $" + y + " Dollars";
         document.getElementById("demo").innerHTML = j;
     } 
 
     if (per == "15%") {
         var k = ( B * 0.15);
-        var y = ( k / p);
+        var y = Math.floor(( k / p)*100 )/100;
         var j = "The Tip Amount To Be Paid is $" + y + " Dollars";
         document.getElementById("demo").innerHTML = j;
     } 
@@ -39,7 +39,7 @@ function allClick() {
     
     if (per == "20%") {
         var k = ( B * 0.2);
-        var y = ( k / p);
+        var y = Math.floor(( k / p)*100 )/100;
         var j = "The Tip Amount To Be Paid is $" + y + " Dollars";
         document.getElementById("demo").innerHTML = j;
         
